54b226c3319d0deb4bd609772d1b3ca9b45cecb8
Zeb Engberg <zebengberg@gmail.com>
2019-12-24 13:03:47 -0500

    color filing changes

--- reforestation.js
+++ reforestation.js
@@ -2,6 +2,9 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
+c.fillStyle = 'rgba(255, 255, 255, 255)';
+c.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
 
 class Forest {
 	constructor() {
@@ -10,6 +13,12 @@ class Forest {
 		this.treeColors = ['MediumSpringGreen', 'Coral'];
 		this.treeArray = [];
 	}
+
+	birthTree() {
+		var x = Math.random() * window.innerWidth;
+		var y = Math.random() * window.innerHeight;
+		var pixelValue = c.getImageData(x, y, 1, 1);
+	}
 }
 
 class Tree {
@@ -17,11 +26,9 @@ class Tree {
 		this.x = Math.random() * window.innerWidth;
 		this.y = Math.random() * window.innerHeight;
 		this.r = 10;
-
 		var index = Math.floor(Math.random() * forest.nTrees);
 		this.growthRate = forest.treeGrowths[index];
 		this.color = forest.treeColors[index];
-
 		this.deathProb = 0;
 		this.crowded = 0;
 		this.isAlive = true;
@@ -31,13 +38,13 @@ class Tree {
 		c.beginPath();
 		c.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
 		c.closePath();
-		c.fillStyle = this.isAlive ? this.color : 'white';
+		c.fillStyle = this.isAlive ? this.color : 'rgba(255, 255, 255, 255)';
 		c.fill();
 	}
 
 	grow() {
 		this.r += this.growthRate * Math.random() / 3;
-		this.deathProb += Math.pow(this.growthRate, 3) / 10000;  // include some randomization
+		this.deathProb += Math.pow(this.growthRate, 3) / 1000;  // include some randomization
 		if (Math.random() < this.deathProb) {
 			this.isAlive = false;
 		}
@@ -55,7 +62,8 @@ for (var i = 0; i < 500; i++) {
 	forest.treeArray.push(tree);
 }
 
-var stuff = function() {
+var update = function() {
+	forest.birthTree();
 	for (var i = 0; i < forest.treeArray.length; i++) {
 		var tree = forest.treeArray[i];
 		if (tree.isAlive) {
@@ -65,4 +73,4 @@ var stuff = function() {
 	}
 }
 
-setInterval(stuff, 10);
+setInterval(update, 100);
