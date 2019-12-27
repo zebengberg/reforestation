c834757053e07026d7b3d6d70f6c9c93bc0d3500
Zeb Engberg <zebengberg@gmail.com>
2019-12-26 23:48:33 -0500

    fixed problems, got into a good functioning state, added metrics

--- reforestation.js
+++ reforestation.js
@@ -1,32 +1,34 @@
-var canvas = document.querySelector('canvas');
+let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var c = canvas.getContext('2d');
+let c = canvas.getContext('2d');
+// Fill the background white so we can paint over dead trees with background color.
 c.fillStyle = 'rgba(255, 255, 255, 255)';
 c.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
 
-
+// Keeps track of all trees and forest-wide parameters.
 class Forest {
 	constructor() {
-		this.nTrees = 4;
-		this.treeGrowths = [.05, .2, .6, .8];
-		this.treeColors = ['MidnightBlue', 'MediumSpringGreen', 'Gold', 'Coral'];
-		this.treeArray = [];
-		this.maxRadius = 100;
-		this.U = window.innerWidth / (2 * this.maxRadius);  // so tree.u <= U
-		this.V = window.innerHeight / (2 * this.maxRadius);  // and tree.v <= V
-		this.boxDict = {};
+		this.nTrees = 5;
+		this.treeGrowths = [.1, .3, .5, .7, .9];
+		this.treeColors = ['MidnightBlue', 'SeaGreen', 'Gold',
+											 'DarkOrange', 'FireBrick'];
+		this.treeArray = [];  // will populate with all trees
+		this.maxRadius = 50;
+		this.boxDict = {};  // boxDict[u, v] will hold trees near (u, v) box-coord
 	}
 
 	// Build the boxes needed to calculated all nearest neighbors.
-	// A given tree goes into 9 different boxes in total.
 	buildBoxDict() {
-		for (var tree of this.treeArray) {
-			var u = tree.u;
-			var v = tree.v;
+		// Reset boxDict.
+		this.boxDict = {};
+		// A given tree goes into 9 different boxes in total.
+		for (let tree of this.treeArray) {
+			let u = tree.u;
+			let v = tree.v;
 			// Not a problem if any of these keys are actually out of bounds.
-			var keys = [[u - 1, v - 1],
+			let keys = [[u - 1, v - 1],
 									[u - 1, v],
 									[u - 1, v + 1],
 									[u, v - 1],
@@ -35,7 +37,7 @@ class Forest {
 									[u + 1, v - 1],
 									[u + 1, v],
 									[u + 1, v + 1]]
-			for (var key of keys) {
+			for (let key of keys) {
 				if (key in this.boxDict) {
 					this.boxDict[key].push(tree);  // key will be converted to string
 				} else {
@@ -45,16 +47,16 @@ class Forest {
 		}
 	}
 
-
   // Get all nearest neighbors by exploiting that trees have a maxRadius.
 	setClosestNeighborDist() {
-		// Clump all trees into small boxes
-		for (var tree of this.treeArray) {
-			var key = [tree.u, tree.v];
+		// For each tree, compare with all other trees in box containing tree.
+		for (let tree of this.treeArray) {
+			let key = [tree.u, tree.v];
 			tree.closestNeighborDist = Infinity;
-			for (var closeTree of this.boxDict[key]) {
-				var candidate = tree.distance(closeTree, true);
-				if (candidate < tree.closestNeighborDist & candidate > 0) {  // excluding same tree
+			for (let closeTree of this.boxDict[key]) {
+				let candidate = tree.distance(closeTree, true);
+				// Need candidate > 0 in order to exclude tree iteself
+				if (candidate < tree.closestNeighborDist & candidate > 0) {
 					tree.closestNeighborDist = candidate;
 				}
 			}
@@ -63,17 +65,18 @@ class Forest {
 
   // Give birth to n new trees if space is available.
 	birthTree(n = 1) {
-		for (var i = 0; i < n; i++) {
-			var x = Math.random() * window.innerWidth;
-			var y = Math.random() * window.innerHeight;
+		for (let i = 0; i < n; i++) {
+			let x = Math.random() * window.innerWidth;
+			let y = Math.random() * window.innerHeight;
 			// Determining if there is a tree at (x, y)
-			var pixelRGBA = c.getImageData(x, y, 1, 1);
-			var colorValue = pixelRGBA.data[0] + pixelRGBA.data[0] + pixelRGBA.data[1];
+			let pixelRGBA = c.getImageData(x, y, 1, 1);
+			let colorValue = pixelRGBA.data[0] + pixelRGBA.data[1]
+											 + pixelRGBA.data[2];
 			if (colorValue > 750) {  // no tree at (x, y)
-				var index = Math.floor(Math.random() * this.nTrees);
-				var growthRate = this.treeGrowths[index];
-				var color = this.treeColors[index];
-				var tree = new Tree(x, y, growthRate, color, this.maxRadius);
+				let index = Math.floor(Math.random() * this.nTrees);
+				let growthRate = this.treeGrowths[index];
+				let color = this.treeColors[index];
+				let tree = new Tree(x, y, growthRate, color, this.maxRadius);
 				this.treeArray.push(tree);  // putting tree into treeArray
 				tree.draw();
 			}
@@ -82,9 +85,9 @@ class Forest {
 
 	// Grow and draw every tree in the forest.
 	grow() {
-		// console.log('Number of trees: ' + this.treeArray.length);
-		for (var i = 0; i < this.treeArray.length; i++) {
-			var tree = this.treeArray[i];
+		// Using a traditional for loop in order to splice out dead trees.
+		for (let i = 0; i < this.treeArray.length; i++) {
+			let tree = this.treeArray[i];
 			if (tree.isAlive) {
 				tree.grow();
 				tree.draw();  // will remove pixels from dead trees
@@ -96,19 +99,40 @@ class Forest {
 
 	// Kill all trees in some big region centered at (x, y)
 	clearCut(x, y) {
-		var threshold = Math.min(window.innerWidth, window.innerHeight) / 4;
-		for (var i = 0; i < this.treeArray.length; i++) {
-			var tree = this.treeArray[i];
-			var distance = Math.sqrt(Math.pow(tree.x - x, 2) + Math.pow(tree.y - y, 2));
-			if (distance < threshold) {
+		let threshold = Math.min(window.innerWidth, window.innerHeight) / 4;
+		for (let tree of this.treeArray) {
+			let dist = Math.sqrt(Math.pow(tree.x - x, 2) + Math.pow(tree.y - y, 2));
+			if (dist < threshold) {
 				tree.isAlive = false;
 				tree.draw();  // remove the dead tree pixels
 			}
 		}
 	}
+
+  // Print summary metrics for the forest.
+	printMetrics() {
+		let colorSummary = {}
+		for (let color of this.treeColors) {
+			colorSummary[color] = 0;
+		}
+		for (let tree of this.treeArray) {
+			let area = Math.PI * Math.pow(tree.r, 2);
+			colorSummary[tree.color] += area;
+		}
+		c.fillStyle = 'rgba(245, 245, 245, 255)';
+		c.fillRect(0, 0, 100, 150);
+		let vertPos = 20;
+		c.font = 'bold 20px sans-serif'
+		for (let color in colorSummary) {
+			c.fillStyle = color;
+			c.fillText(Math.floor(colorSummary[color]), 20, vertPos);
+			vertPos += 30;
+		}
+	}
 }
 
 
+// Keeps track of individual trees.
 class Tree {
 	constructor(x, y, growthRate, color, maxRadius) {
 		this.x = x;
@@ -127,12 +151,12 @@ class Tree {
 
 	// Grow the tree and determine if stil alive.
 	grow() {
-		// Growing r with some random noise.
-		this.r += this.growthRate * Math.random() / 1;
-		// Clipping r so that it isn't larger than maxRadius or overlap neighbor.
-		this.r = Math.min(this.r, this.maxRadius, this.closestNeighborDist);
-
-		this.deathProb += Math.pow(this.growthRate, 3) / 50000;
+		// Growing r with some random noise. Don't grow into neighbors.
+		if ((this.r < this.maxRadius) && (this.r < this.closestNeighborDist)) {
+			this.r += this.growthRate * Math.random() / 5;
+		}
+		// Square the growthRate to bring it even closer to 0.
+		this.deathProb += Math.pow(this.growthRate, 2) / Math.pow(10, 5);
 		if (Math.random() < this.deathProb) {  // tree dies
 			this.isAlive = false;
 		}
@@ -144,7 +168,7 @@ class Tree {
 		if (this.isAlive) {
 			c.fillStyle = this.color;
 		} else {
-			this.r += 1  // pad the blank circle so no leftovers appear
+			this.r += 2  // pad the blank circle so no leftovers after filled white
 			c.fillStyle = 'rgba(255, 255, 255, 255)';
 		}
 		c.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
@@ -156,34 +180,39 @@ class Tree {
 		c.fill();
 	}
 
-	// Get distance between this tree and another tree
+	// Get distance between this tree and another tree.
 	distance(tree, toEdge = false) {
-		var distance = Math.sqrt(Math.pow(this.x - tree.x, 2)
-		                         + Math.pow(this.x - tree.x, 2));
-	  if (toEdge) {
+		let distance = Math.sqrt(Math.pow(this.x - tree.x, 2)
+		                         + Math.pow(this.y - tree.y, 2));
+	  if (toEdge) {  // get distance to edge of circle
 			return distance - tree.r;  // subtract off the radius of second tree
-		} else {
+		} else {  // get distance to center of tree
 			return distance;
 		}
 	}
 }
 
 
-var forest = new Forest;
+let forest = new Forest;
 
 // Clear cut after user click.
-var doOnClick = function(event) {
-	var x = event.clientX;
-	var y = event.clientY;
+let doOnClick = function(event) {
+	let x = event.clientX;
+	let y = event.clientY;
 	forest.clearCut(x, y);
 }
 canvas.addEventListener('click', doOnClick);
 
 // Animate the forest.
-var update = function() {
-	forest.birthTree();
+let update = function() {
+	// Increasing the number of births will result in smaller trees. The number
+	// of births and the constant in front of the deathProb should be inverse.
+	forest.birthTree(3);
 	forest.buildBoxDict();
 	forest.setClosestNeighborDist();
 	forest.grow();
+	forest.printMetrics();
 };
-setInterval(update, 200);
+
+// Applying function update after every n milliseconds.
+setInterval(update, 50);
